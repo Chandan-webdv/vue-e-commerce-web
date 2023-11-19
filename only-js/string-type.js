052ac2b5014c2670898b2258fcf7d101a1a956ab
chandand-gintaa <chandandas@asconsoftech.com>
2023-11-19 23:35:03 +0530

    first commit

---
+++ only-js/string-type.js
@@ -0,0 +1,8 @@
+var userName = prompt("Enter your name");
+var ahe = parseInt("Enter your age");
+
+var msg1="hello !" + "" +username +"" +"you will be"+""+(age+1)+""+"next year."<br>
+var msg2=`hello !${userName} you will be ${age+1}nextyear.`;
+
+document.write(mag1);
+document.write(msg2);
